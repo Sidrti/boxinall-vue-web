f1459cf492a5642650149ae5560a235ab9a8900e
unknown <harshitmishra5348@gmail.com>
2024-07-25 14:54:02 +0530

    mobile route reset

--- src/components/Mobile/AIFeatures.vue
+++ src/components/Mobile/AIFeatures.vue
@@ -3,16 +3,16 @@
 
 
   <div class="container">
-    <h1 class="text-white text-center mb-4">
+    <h1 class="text-black text-start mb-4">
       Leveraging Emerging Technologies to Enhance Your Digital Journey and Drive
       Increased Value
     </h1>
-    <h5 class="text-white text-center mb-4" style="opacity: 0.8">
+    <p class="text-black text-start mb-4" style="font-weight: 500;font-size: 20px;">
       Introducing mobility for your users is the most prominent way of staying
       ahead of the curve. Our mobile app development services landscape is
       backed by disruptive technologies that can increase your business efficacy
       and enhance operational efficiency and productivity.
-    </h5>
+    </p>
     <div class="row justify-content-center">
       <!-- Loop through industryData array to generate cards -->
       <div
@@ -20,20 +20,20 @@
         :key="industry.name"
         class="col-lg-4 col-md-6 mb-4"
       >
-        <div class="card bg-dark p-4">
-          <div class="d-flex align-items-center mb-3">
+        <div class="card p-4">
+          <div class="image-card d-flex align-items-center mb-3">
             <img
               class="small-icon mr-3"
               :src="industry.icon"
               :alt="industry.name + ' Icon'"
             />
           </div>
-          <h2 class="text-white card-title mb-4" style="opacity: 0.9">
+          <h3 class="text-black card-title mb-4" style="opacity: 0.9">
             {{ industry.name }}
-          </h2>
-          <h6 class="text-white card-text" style="opacity: 0.8">
+          </h3>
+          <p class="text-black card-text" style="font-weight: 400;">
             {{ industry.description }}
-          </h6>
+          </p>
         </div>
       </div>
     </div>
@@ -49,21 +49,21 @@ export default {
       industryData: [
         {
           name: "Artificial Intelligence",
-          icon: require("@/assets/images/industry/ic_glass_bag.png"), // Replace with actual icon path
+          icon: require("@/assets/images/ai/ai-img.png"), // Replace with actual icon path
           description:
             "Our highly skilled mobile app developers have the expertise to revolutionize and propel your business forward by integrating next-gen artificial intelligence solutions. With our advanced AI capabilities, you can automate processes, drive innovation, and unlock new possibilities for your business.",
           route: "/ecommerce",
         },
         {
           name: "Internet of Things",
-          icon: require("@/assets/images/industry/ic_glass_message.png"), // Replace with actual icon path
+          icon: require("@/assets/images/ai/IoT-img.png"), // Replace with actual icon path
           description:
             "We leverage IoT technology by seamlessly integrating connected devices, sensors, and data analytics with mobile apps. Our effective IoT mobile app development services collect and analyze big data, enabling you to streamline operations.",
           route: "/education",
         },
         {
           name: "AR/VR",
-          icon: require("@/assets/images/industry/ic_glass_message.png"), // Replace with actual icon path
+          icon: require("@/assets/images/ai/AR-VR-img.png"), // Replace with actual icon path
           description:
             "Our team of skilled mobile app developers excels in creating immersive experiences through cutting-edge technologies. We harness the power of AR/VR to enhance brand awareness and foster business growth, making it a valuable asset for your business success.",
           route: "/medical",
@@ -78,7 +78,8 @@ export default {
 
 <style scoped>
 .main{
-  background-color: black;
+  background-color: white;
+  margin-bottom: 100px;
 }
 .container{
   padding-top: 100px;
@@ -88,10 +89,12 @@ export default {
   transition: transform 0.2s;
   height: 500px;
   border: 1px solid white;
+
 }
 
 .card:hover {
-  transform: translateY(-5px);
+  background-color: rgb(236, 243, 245);
+
 }
 
 .card-img-top {
@@ -99,8 +102,24 @@ export default {
   object-fit: cover;
 }
 
+.image-card {
+  width: 40px;
+  height: 40px;
+  background-color: #514f57;
+  border-radius: 10px;
+  justify-content: center;
+
+}
+
 .small-icon {
-  width: 40px; /* Adjust size as needed */
-  height: auto;
+/* Adjust size as needed */
+  height: 60%;
+  width: 60%;
+  object-fit: contain;
+
+
+
+
+
 }
 </style>
